8f72ca69a1337894868c2c14270be90b47d92a81
KkEditor <32573671+KkEditor@users.noreply.github.com>
2020-05-01 13:50:19 +0700

    only mean & std with super clean format

--- haar_test.py
+++ haar_test.py
@@ -5,6 +5,8 @@ import pywt
 import math
 import os
 import scipy.stats as stats
+import pandas as pd
+from scipy.fftpack import dct
 def von_neumann_entropy(density_matrix, cutoff=10):
     x = np.mat(density_matrix)
     one = np.identity(x.shape[0])
@@ -25,17 +27,18 @@ def calculate_statistics(list_values):
     # no_zero_crossings = len(zero_crossing_indices)
     # mean_crossing_indices = np.nonzero(np.diff(np.array(list_values) > np.nanmean(list_values)))[0]
     # no_mean_crossings = len(mean_crossing_indices)
-    n5 = np.percentile(list_values, 5)
-    n25 = np.percentile(list_values, 25)
-    median = np.percentile(list_values, 50)
+    # n5 = np.percentile(list_values, 5)
+    # n25 = np.percentile(list_values, 25)
+    # median = np.percentile(list_values, 50)
     mean = np.mean(list_values)
     std = np.std(list_values)
-    var = np.var(list_values)
-    rms = np.mean(np.sqrt(list_values ** 2))
-    mad=np.array(stats.median_absolute_deviation(np.std(list_values),axis=None),dtype=np.float64)
-    coef=np.array(stats.variation(np.var(list_values)))
-    # print(mad)
-    return n5, n25, median, mean, std, var, rms,mad,coef
+    # var = np.var(list_values)
+    # rms = np.mean(np.sqrt(list_values ** 2))
+    # mad=np.array(stats.median_absolute_deviation(np.std(list_values),axis=None),dtype=np.float64)
+    # coef=np.array(stats.variation(np.var(list_values)))
+    # # print(mad)
+    # return n5, n25, median, mean, std, var, rms,mad,coef
+    return mean,std
 
 
 def get_features(list_values):
@@ -49,22 +52,26 @@ def get_features(list_values):
 def haar_extract(img,size):
     img=cv2.resize(img,(size[0],size[1]))
     feature=[]
-    cof=pywt.wavedec2(img,'haar')
-    out = [item for t in cof for item in t]
-    for e in out:
+    # cof=pywt.wavedec2(img,'haar')
+    # out = [item for t in cof for item in t]
+    cA,cN=pywt.dwt2(img,'haar')
+    sin=dct(img)
+    feature.append(get_features(sin))
+    for e in cN:
         feature.append(get_features(e))
-    return np.ndarray.flatten(np.array(feature))
+    return np.array(list(map('{:.6f}'.format,np.ndarray.flatten(np.array(feature)))))
 
 
 def main():
     start = timeit.default_timer()
     path="C:/Users/kk/Desktop/crop/"
+    test=pd.read_csv("test.csv")
     namelist=os.listdir(path)
     for i in namelist:
         img=cv2.imread(path+i,0)
         fea = haar_extract(img, (256, 256))
         print(fea)
-
+        # print(fea)
 
 
     stop1 = timeit.default_timer()
